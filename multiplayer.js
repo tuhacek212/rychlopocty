b211b1b888458552c745a145a264c5708eb0466f
tuhacek212 <tuhacek212@gmail.com>
2025-10-13 11:33:42 +0200

    Add files via upload

--- multiplayer.js
+++ multiplayer.js
@@ -31,7 +31,17 @@ export class MultiplayerManager {
                     iceServers: [
                         { urls: 'stun:stun.l.google.com:19302' },
                         { urls: 'stun:stun1.l.google.com:19302' },
-                        { urls: 'stun:stun2.l.google.com:19302' }
+                        { urls: 'stun:stun2.l.google.com:19302' },
+                        {
+                            urls: 'turn:openrelay.metered.ca:80',
+                            username: 'openrelayproject',
+                            credential: 'openrelayproject'
+                        },
+                        {
+                            urls: 'turn:openrelay.metered.ca:443',
+                            username: 'openrelayproject',
+                            credential: 'openrelayproject'
+                        }
                     ]
                 }
             });
